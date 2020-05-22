d8f2bf906c313006bbf6df029cc811e0e8916f2b
ashikahmedfahim <41462787+ashikahmedfahim@users.noreply.github.com>
2020-05-22 15:40:03 +0600

    update

--- hp2/doctor_service_panel.cs
+++ hp2/doctor_service_panel.cs
@@ -12,9 +12,19 @@ namespace hp2
 {
     public partial class doctor_service_panel : UserControl
     {
+
+        DBAccess dbobj;
+        DataTable dtUser;
         public doctor_service_panel()
         {
             InitializeComponent();
+        }
+
+        private void load() {
+            dbobj = new DBAccess();
+            dtUser = new DataTable();
+
+        
         }
 
         private void comboBoxName_SelectedIndexChanged(object sender, EventArgs e)
@@ -24,7 +34,7 @@ namespace hp2
 
         private void doctor_service_panel_Load(object sender, EventArgs e)
         {
-
+            load();
         }
     }
 }
